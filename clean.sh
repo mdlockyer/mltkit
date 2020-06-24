84c5b8889101a18f874a5e19a4ce2270df835c52
Michael Lockyer <mdlockyer@gmail.com>
2020-06-24 00:11:20 -0500

    Now works on MacOS

--- clean.sh
+++ clean.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-PROJECT_DIR=$(dirname $(readlink -f "$0"))
+PROJECT_DIR=$(dirname "$0")
 BUILD_DIR=$PROJECT_DIR/build
-rm -r $BUILD_DIR
-echo Removed build directory: $BUILD_DIR
+rm -r $BUILD_DIR && echo Removed build directory: $BUILD_DIR
 DIST_DIR=$PROJECT_DIR/dist
-rm -r $DIST_DIR
-echo Removed dist directory: $DIST_DIR
+rm -r $DIST_DIR && echo Removed dist directory: $DIST_DIR
