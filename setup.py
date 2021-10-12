7f8b00f5857859c6dda1595c41431c8dfff96f33
Michael Lockyer <mdlockyer@gmail.com>
2021-10-12 18:04:48 -0500

    Working on build script

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
+import shutil
+from pathlib import Path
 
 from setuptools import setup, find_packages
-import mltk
+import src as mltkit
 
-name = 'mltk'
-description = ''
-long_description = ''
-url = 'https://github.com/mdlockyer/mltk'
+name = 'mltkit'
+description = 'A toolkit for ML boilerplate.'
+url = 'https://github.com/mdlockyer/mltkit'
 author = 'Michael Lockyer'
 author_email = 'mdlockyer@gmail.com'
-version = mltk.__version__
+version = src.__version__
 license_type = 'MIT License'
 classifiers = (
     "Programming Language :: Python :: 3",
@@ -19,7 +20,14 @@ classifiers = (
 install_requires = ['PrintTags']
 packages = find_packages()
 
-setup(name=name, description=description, long_description=long_description,
-      version=version, url=url, author=author, author_email=author_email,
-      license=license_type, classifiers=classifiers, packages=packages,
-      python_requires='>=3.6')
+proj_dir = Path(__file__).parent.resolve()
+src_path = proj_dir.joinpath('src')
+dest_path = proj_dir.joinpath(name)
+try:
+    shutil.copytree(src_path, dest_path)
+    setup(name=name, description=description, version=version,
+          url=url, author=author, author_email=author_email,
+          license=license_type, classifiers=classifiers,
+          packages=packages, python_requires='>=3.6')
+finally:
+    shutil.rmtree(dest_path)
