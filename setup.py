6d3ea2f48aacebb8e04667c87cbf265b46139e45
Michael Lockyer <mdlockyer@gmail.com>
2021-10-13 13:21:17 -0500

    Rename to mltkit

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import shutil
 from pathlib import Path
 
 from setuptools import setup, find_packages
-import src as mltkit
+import mltkit
 
 name = 'mltkit'
 description = 'A toolkit for ML boilerplate.'
 url = 'https://github.com/mdlockyer/mltkit'
 author = 'Michael Lockyer'
 author_email = 'mdlockyer@gmail.com'
-version = src.__version__
+version = mltkit.__version__
 license_type = 'MIT License'
 classifiers = (
     "Programming Language :: Python :: 3",
@@ -20,14 +20,7 @@ classifiers = (
 install_requires = ['PrintTags']
 packages = find_packages()
 
-proj_dir = Path(__file__).parent.resolve()
-src_path = proj_dir.joinpath('src')
-dest_path = proj_dir.joinpath(name)
-try:
-    shutil.copytree(src_path, dest_path)
-    setup(name=name, description=description, version=version,
-          url=url, author=author, author_email=author_email,
-          license=license_type, classifiers=classifiers,
-          packages=packages, python_requires='>=3.6')
-finally:
-    shutil.rmtree(dest_path)
+setup(name=name, description=description, version=version,
+      url=url, author=author, author_email=author_email,
+      license=license_type, classifiers=classifiers,
+      packages=packages, python_requires='>=3.6')
