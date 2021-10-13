71f2f4b9ece9168f1dd9d07f7fd130a6d143ffb0
Michael Lockyer <mdlockyer@gmail.com>
2021-10-13 13:20:50 -0500

    Cleanup egg-info dir

--- clean.sh
+++ clean.sh
@@ -4,3 +4,5 @@ BUILD_DIR=$PROJECT_DIR/build
 rm -r $BUILD_DIR && echo Removed build directory: $BUILD_DIR
 DIST_DIR=$PROJECT_DIR/dist
 rm -r $DIST_DIR && echo Removed dist directory: $DIST_DIR
+EGG_INFO_DIR=$PROJECT_DIR/*.egg-info
+rm -r $EGG_INFO_DIR && echo Removed egg-info directories: $EGG_INFO_DIR
